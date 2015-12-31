e4d1d9c1aa567ab27734cf6796abe44cf9dc4c2b
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:42:50 +1300

    Converted to PSR-2

--- code/forms/EditableAncestryField.php
+++ code/forms/EditableAncestryField.php
@@ -5,60 +5,65 @@
  * @subpackage relatives
  */
 
-class EditableAncestryField extends EditableFormField {
+class EditableAncestryField extends EditableFormField
+{
 
-	static $singular_name = 'Ancestry field';
+    public static $singular_name = 'Ancestry field';
 
-	static $plural_name = 'Ancestry fields';
+    public static $plural_name = 'Ancestry fields';
 
-	function getFieldConfiguration() {
-		$fields = parent::getFieldConfiguration();
-		return $fields;
-	}
+    public function getFieldConfiguration()
+    {
+        $fields = parent::getFieldConfiguration();
+        return $fields;
+    }
 
-	function getFormField() {
-		return new AncestryField($this->Name, $this->Title);
-	}
+    public function getFormField()
+    {
+        return new AncestryField($this->Name, $this->Title);
+    }
 
-	/**
-	 * Return the validation information related to this field. This is
-	 * interrupted as a JSON object for validate plugin and used in the
-	 * PHP.
-	 *
-	 * @see http://docs.jquery.com/Plugins/Validation/Methods
-	 * @return Array
-	 */
-	public function getValidation() {
-		$options = array();
-		return $options;
-	}
+    /**
+     * Return the validation information related to this field. This is
+     * interrupted as a JSON object for validate plugin and used in the
+     * PHP.
+     *
+     * @see http://docs.jquery.com/Plugins/Validation/Methods
+     * @return Array
+     */
+    public function getValidation()
+    {
+        $options = array();
+        return $options;
+    }
 
-	/**
-	 * Return the Value of this Field
-	 *
-	 * @return String
-	 */
-	function getValueFromData($data) {
-		$returnValue = "";
-		$value = (isset($data[$this->Name])) ? $data[$this->Name] : false;
-		if($value) {
-			if(is_array($value)) {
-				foreach($value as $key => $dataEntered) {
-					$key = str_replace("Field", "", $key);
-					$key = str_replace("m", "mother-", $key);
-					$key = str_replace("f", "father-", $key);
-					if(!$dataEntered) {
-						$dataEntered = "---";
-					}
-					$returnValue .= "<br />$key: $dataEntered ";
-				}
-			}
-		}
-		return $returnValue;
-	}
+    /**
+     * Return the Value of this Field
+     *
+     * @return String
+     */
+    public function getValueFromData($data)
+    {
+        $returnValue = "";
+        $value = (isset($data[$this->Name])) ? $data[$this->Name] : false;
+        if ($value) {
+            if (is_array($value)) {
+                foreach ($value as $key => $dataEntered) {
+                    $key = str_replace("Field", "", $key);
+                    $key = str_replace("m", "mother-", $key);
+                    $key = str_replace("f", "father-", $key);
+                    if (!$dataEntered) {
+                        $dataEntered = "---";
+                    }
+                    $returnValue .= "<br />$key: $dataEntered ";
+                }
+            }
+        }
+        return $returnValue;
+    }
 
-	public function Icon() {
-		return 'userforms_relatives/images/icons/' . strtolower($this->class) . '.png';
-	}
-
+    public function Icon()
+    {
+        return 'userforms_relatives/images/icons/' . strtolower($this->class) . '.png';
+    }
 }
--- code/forms/OffSpringField.php
+++ code/forms/OffSpringField.php
@@ -4,174 +4,188 @@
  * @package userforms
  * @subpackage relatives
  */
-class OffSpringField extends FormField {
+class OffSpringField extends FormField
+{
 
-	/**
-	 * @var nameField
-	 */
-	protected $headerField = null;
+    /**
+     * @var nameField
+     */
+    protected $headerField = null;
 
-	/**
-	 * @var nameField
-	 */
-	protected $nameField = null;
+    /**
+     * @var nameField
+     */
+    protected $nameField = null;
 
-	/**
-	 * @var dobField
-	 */
-	protected $dobField = null;
+    /**
+     * @var dobField
+     */
+    protected $dobField = null;
 
-	/**
-	 * @var dobField
-	 */
-	protected $sexField = null;
-
-
-	function __construct($name, $title = null, $value = ""){
-		$this->headerField = new HeaderField($name . '[header]', $title, 4);
-		$this->nameField = new TextField($name . '[name]', "Name");
-		$this->dobField = new TextField($name . '[dob]', "Date of Birth (e.g. 31/01/1986)");
-		$this->sexField = new OptionsetField($name . '[sex]', false, array("female", "male"));
-		$this->headerField->addExtraClass("header");
-		$this->nameField->addExtraClass("name");
-		$this->dobField->addExtraClass("dob");
-		$this->sexField->addExtraClass("sexField");
-
-		parent::__construct($name, $title, $value);
-	}
-
-	function setForm($form) {
-		parent::setForm($form);
-
-		$this->headerField->setForm($form);
-		$this->nameField->setForm($form);
-		$this->dobField->setForm($form);
-		$this->sexField->setForm($form);
-	}
-
-	function Field() {
-		//Requirements::css();
-		//Requirements::javascript();
-		return $this->headerField->SmallFieldHolder() . $this->nameField->SmallFieldHolder() . $this->dobField->SmallFieldHolder() . $this->sexField->SmallFieldHolder() . '<div class="clear"><!-- --></div>';
-	}
-
-	/**
-	 * Sets the internal value to ISO date format.
-	 *
-	 * @param string|array $val String expects an ISO date format. Array notation with 'date' and 'time'
-	 *  keys can contain localized strings. If the 'dmyfields' option is used for {@link nameField},
-	 *  the 'date' value may contain array notation was well (see {@link nameField->setValue()}).
-	 */
-	function setValue($val) {
-		if(empty($val)) {
-			$this->nameField->setValue(null);
-			$this->dobField->setValue(null);
-			$this->sexField->setValue(null);
-		}
-		else {
-			// String setting is only possible from the database, so we don't allow anything but ISO format
-			if(is_string($val)) {
-				//TO DO
-			}
-			// Setting from form submission
-			elseif(is_array($val) && array_key_exists('name', $val) && array_key_exists('dob', $val) && array_key_exists('sex', $val)) {
-				$this->nameField->setValue($val['name']);
-				$this->dobField->setValue($val['dob']);
-				$this->sexField->setValue($val['sex']);
-			}
-			else {
-				$this->nameField->setValue($val);
-				$this->dobField->setValue($val);
-				$this->sexField->setValue($val);
-			}
-		}
-	}
-
-	function dataValue() {
-		$valName = $this->nameField->dataValue();
-		$valDob = $this->dobField->dataValue();
-		$valSex = $this->sexField->dataValue();
-
-		$array = array("name" => $valName, "dob" => $valDob, "sex" => $valSex);
-		return implode("|", $array);
-	}
+    /**
+     * @var dobField
+     */
+    protected $sexField = null;
 
 
-	/**
-	 * @return nameField
-	 */
-	function getHeaderField() {
-		return $this->headerField;
-	}
+    public function __construct($name, $title = null, $value = "")
+    {
+        $this->headerField = new HeaderField($name . '[header]', $title, 4);
+        $this->nameField = new TextField($name . '[name]', "Name");
+        $this->dobField = new TextField($name . '[dob]', "Date of Birth (e.g. 31/01/1986)");
+        $this->sexField = new OptionsetField($name . '[sex]', false, array("female", "male"));
+        $this->headerField->addExtraClass("header");
+        $this->nameField->addExtraClass("name");
+        $this->dobField->addExtraClass("dob");
+        $this->sexField->addExtraClass("sexField");
 
-	/**
-	 * @return nameField
-	 */
-	function getNameField() {
-		return $this->nameField;
-	}
+        parent::__construct($name, $title, $value);
+    }
 
-	/**
-	 * @return dobField
-	 */
-	function getDobField() {
-		return $this->dobField;
-	}
+    public function setForm($form)
+    {
+        parent::setForm($form);
 
-	/**
-	 * @return sexField
-	 */
-	function getSexField() {
-		return $this->sexField;
-	}
+        $this->headerField->setForm($form);
+        $this->nameField->setForm($form);
+        $this->dobField->setForm($form);
+        $this->sexField->setForm($form);
+    }
+
+    public function Field()
+    {
+        //Requirements::css();
+        //Requirements::javascript();
+        return $this->headerField->SmallFieldHolder() . $this->nameField->SmallFieldHolder() . $this->dobField->SmallFieldHolder() . $this->sexField->SmallFieldHolder() . '<div class="clear"><!-- --></div>';
+    }
+
+    /**
+     * Sets the internal value to ISO date format.
+     *
+     * @param string|array $val String expects an ISO date format. Array notation with 'date' and 'time'
+     *  keys can contain localized strings. If the 'dmyfields' option is used for {@link nameField},
+     *  the 'date' value may contain array notation was well (see {@link nameField->setValue()}).
+     */
+    public function setValue($val)
+    {
+        if (empty($val)) {
+            $this->nameField->setValue(null);
+            $this->dobField->setValue(null);
+            $this->sexField->setValue(null);
+        } else {
+            // String setting is only possible from the database, so we don't allow anything but ISO format
+            if (is_string($val)) {
+                //TO DO
+            }
+            // Setting from form submission
+            elseif (is_array($val) && array_key_exists('name', $val) && array_key_exists('dob', $val) && array_key_exists('sex', $val)) {
+                $this->nameField->setValue($val['name']);
+                $this->dobField->setValue($val['dob']);
+                $this->sexField->setValue($val['sex']);
+            } else {
+                $this->nameField->setValue($val);
+                $this->dobField->setValue($val);
+                $this->sexField->setValue($val);
+            }
+        }
+    }
+
+    public function dataValue()
+    {
+        $valName = $this->nameField->dataValue();
+        $valDob = $this->dobField->dataValue();
+        $valSex = $this->sexField->dataValue();
+
+        $array = array("name" => $valName, "dob" => $valDob, "sex" => $valSex);
+        return implode("|", $array);
+    }
 
 
-	function validate($validator) {
-		$dateValid = $this->nameField->validate($validator);
-		$timeValid = $this->dobField->validate($validator);
-		$sexValid = $this->sexField->validate($validator);
+    /**
+     * @return nameField
+     */
+    public function getHeaderField()
+    {
+        return $this->headerField;
+    }
 
-		return ($dateValid && $timeValid && $sexValid);
-	}
+    /**
+     * @return nameField
+     */
+    public function getNameField()
+    {
+        return $this->nameField;
+    }
 
-	function jsValidation() {
-		return $this->nameField->jsValidation() . $this->dobField->jsValidation() . $this->sexField->jsValidation() ;
-	}
+    /**
+     * @return dobField
+     */
+    public function getDobField()
+    {
+        return $this->dobField;
+    }
 
-	function performReadonlyTransformation() {
-		$field = new OffSpringField_Readonly($this->name, $this->title, $this->dataValue());
-		$field->setForm($this->form);
-		return $field;
-	}
+    /**
+     * @return sexField
+     */
+    public function getSexField()
+    {
+        return $this->sexField;
+    }
+
+
+    public function validate($validator)
+    {
+        $dateValid = $this->nameField->validate($validator);
+        $timeValid = $this->dobField->validate($validator);
+        $sexValid = $this->sexField->validate($validator);
+
+        return ($dateValid && $timeValid && $sexValid);
+    }
+
+    public function jsValidation()
+    {
+        return $this->nameField->jsValidation() . $this->dobField->jsValidation() . $this->sexField->jsValidation() ;
+    }
+
+    public function performReadonlyTransformation()
+    {
+        $field = new OffSpringField_Readonly($this->name, $this->title, $this->dataValue());
+        $field->setForm($this->form);
+        return $field;
+    }
 }
 
 /**
 
  */
-class OffSpringField_Readonly extends OffSpringField {
+class OffSpringField_Readonly extends OffSpringField
+{
 
-	protected $readonly = true;
+    protected $readonly = true;
 
-	function Field() {
-		$valName = $this->nameField->dataValue();
-		$valDob = $this->dobField->dataValue();
-		$valSex = $this->sexField->dataValue();
-		if($valDate && $valTime && $valSex) {
-			$val = $valName." ".$valDob." ".$valSex;
-		}
-		else {
-			// TODO Localization
-			$val = '<i>(not set)</i>';
-		}
+    public function Field()
+    {
+        $valName = $this->nameField->dataValue();
+        $valDob = $this->dobField->dataValue();
+        $valSex = $this->sexField->dataValue();
+        if ($valDate && $valTime && $valSex) {
+            $val = $valName." ".$valDob." ".$valSex;
+        } else {
+            // TODO Localization
+            $val = '<i>(not set)</i>';
+        }
 
-		return "<span class=\"readonly\" id=\"" . $this->id() . "\">$val</span>";
-	}
+        return "<span class=\"readonly\" id=\"" . $this->id() . "\">$val</span>";
+    }
 
-	function jsValidation() {
-		return null;
-	}
+    public function jsValidation()
+    {
+        return null;
+    }
 
-	function validate($validator) {
-		return true;
-	}
+    public function validate($validator)
+    {
+        return true;
+    }
 }

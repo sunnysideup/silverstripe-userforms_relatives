1b741dcac72105a95955acf93bcf384b31cc0b91
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 14:00:04 +1300

    PATCH: module clean-up

--- _config.php
+++ _config.php
@@ -11,4 +11,3 @@
 //===================---------------- START userforms_relatives MODULE ----------------===================
 //AncestryField::set_array_of_ancestors($a);
 //===================---------------- END userforms_relatives MODULE ----------------===================
-
